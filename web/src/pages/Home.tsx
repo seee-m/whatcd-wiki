a3c3da1175cca82872a8f9e352109b1c89d58aeb
Connor MacDonald <44072964+seee-m@users.noreply.github.com>
2026-08-26 15:41:07 +0100

    Update what.tv homepage blurb to 400k releases
    The Discogs bulk import now matches enough releases that the old
    300k figure undersells it.

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -72,7 +72,7 @@ export function Home() {
             <Link to="/list">Lists</Link> &mdash; Create shareable lists from the what.cd archive
           </li>
           <li>
-            <Link to="/tv">what.tv</Link> &mdash; Surf over 300k releases ready to play instantly{' '}
+            <Link to="/tv">what.tv</Link> &mdash; Surf over <strong>400k</strong> releases ready to play instantly{' '}
             <img src={newGif} alt="New" className="new-badge" />
           </li>
         </ul>
